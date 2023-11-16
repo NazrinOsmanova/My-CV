fcfeeba3bb1d671ee91bcd210f32687b67c010aa
NazrinOsmanova <nazrinosmanovaa@gmail.com>
2023-11-17 01:42:26 +0400

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-// import './App.css';
-// import './App.css';
-import About from './Component/About';
 import UserCV from './Component/UserCV';
 import './Component/CV.css';
 
 function App() {
-  // burada js kodlari
-
   return (
     <>
       <div className='esas'>
--- src/Component/Experience.js
+++ src/Component/Experience.js
@@ -7,11 +7,13 @@ const Experience = () => {
             <h2>Experience</h2>
             <div className='items'>
                 <div className="item">
-                    <h3>Modern Youth Development Support Public Union</h3>
+                    <h3>Coordinator</h3>
+                    <h4>"Modern Youth Development Support" Public Union</h4>
                     <p>23.05.2023 - Current Date</p>
                 </div>
                 <div className="item">
                     <h3>Project Manager</h3>
+                    <h4>"Aurora" Startup Team</h4>
                     <p>05.03.2023 - 28.03.2023</p>
                 </div>
             </div>
